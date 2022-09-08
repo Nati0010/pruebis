0fdc3487237bf826c8c336a8c6b224490dc47b40
Natalia <nataliarojaslon9@gmail.com>
2022-09-08 10:08:05 -0500

    js

---
+++ prueba.js
@@ -0,0 +1,6 @@
+function funcion2(){
+    let x=prompt("digite el numero 1");
+    let y=5;
+    let z=parseInt(x)+y;
+    document.getElementById("prueba1").innerHTML="apuntado ok, el valor de z es:"+z;
+}
